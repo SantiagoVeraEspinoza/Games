123d12a7a09c58f3d4a118f8d1f07a819dfd3025
TitanPro47 <67120257+TitanPro47@users.noreply.github.com>
2020-07-15 12:33:32 -0500

    Update Main.cpp
    ChangeLog:
    -Added function to save tetrominos
    -Added surround squares to renderized support tetrominos

--- OLC/OLC_ConsoleGameEngine/Tetris/Main.cpp
+++ OLC/OLC_ConsoleGameEngine/Tetris/Main.cpp
@@ -8,11 +8,13 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include <math.h>
 #include "olcConsoleGameEngineSDL.h"
 using namespace std;
 
 class Juego : public olcConsoleGameEngine{
+    
 private:
     
     int long score;
@@ -31,6 +33,7 @@ private:
     int long UserTetrominoArraySize=0;
     int long MeshTetrominoArraySize=0;
     int long MeshIndex=0;
+    int long SavedArraySize=0;
     
     int const static Block_Size=4;
     
@@ -43,6 +46,8 @@ private:
         short Colour;
     };
     
+    int Saved_Type;
+    bool HasBeenPressed=false;
     int Next_Type=5;
     
     vector <Cubes> Tetromino;
@@ -53,11 +58,21 @@ private:
     float Pos_X=ScreenWidth()-8;
     float Pos_Y=28;
     
+    int OffSet_X_SavedTetromino=0;
+    int OffSet_Y_SavedTetromino=0;
+    int OffSet_X_NextTetromino=0;
+    int OffSet_Y_NextTetromino=0;
+    
     float User_Flag=0;
     float User_Y=0;
     
+    int nSample=0;
+    string MainTheme = "/Users/santiagovera/Desktop/C++/Juegos/Tetris/Tetris/en.lproj/Tetris.wav";
+    
 public:
-    Juego(){}
+    Juego(){
+        EnableSound();
+    }
     ~Juego(){}
     
     void DrawField(){
@@ -71,15 +86,37 @@ public:
         int x3=x2;
         int y3=y2-Block_Size;
         int x4=x2+FieldWidth-Block_Size;
-        int y4=y2;
+        int y4=y2+1;
         int x5=x4-Block_Size;
         int y5=y1;
-        int x6=x4;
+        int x6=x4+1;
         int y6=y4;
         
-        Fill(x1,y1,x2,y2,PIXEL_SOLID,FG_WHITE);
+        Fill(x1,y1-1,x2,y2+1,PIXEL_SOLID,FG_WHITE);
         Fill(x3,y3,x4,y4,PIXEL_SOLID,FG_WHITE);
-        Fill(x5,y5,x6,y6,PIXEL_SOLID,FG_WHITE);
+        Fill(x5,y5-1,x6,y6,PIXEL_SOLID,FG_WHITE);
+        
+        for (int i=y1; i<y2; i=i+4){
+            DrawLine(x1+Block_Size, i, x4-1-Block_Size, i, FG_BLACK);
+        }
+        for (int j=x1+Block_Size; j<x4-Block_Size+1; j=j+4){
+            DrawLine(j, y1, j, y2-Block_Size, FG_BLACK);
+        }
+        
+        DrawLine(x5, y5-1, x5, y5, FG_BLACK);
+        
+        DrawSquareNextToField(OffSet_X_SavedTetromino, OffSet_Y_SavedTetromino);
+        DrawSquareNextToField(OffSet_X_NextTetromino, OffSet_Y_NextTetromino);
+        DrawLine(OffSet_X_NextTetromino+(Block_Size*4)+1, OffSet_Y_NextTetromino, OffSet_X_NextTetromino+1+(Block_Size*4), OffSet_Y_NextTetromino+(4*Block_Size));
+        
+    }
+    
+    void DrawSquareNextToField(int x, int y){
+        DrawLine(x, OffSet_Y_SavedTetromino-1, x+1+(Block_Size*4), y-1);
+        DrawLine(y+1+(Block_Size*4), OffSet_Y_SavedTetromino, OffSet_X_SavedTetromino+1+(Block_Size*4), y+(Block_Size*4));
+        DrawLine(x+1+(Block_Size*4), y+(Block_Size*4)+1, x, y+(4*Block_Size)+1);
+        DrawLine(x-1, y+(4*Block_Size)+1, x-1, y-1);
+        
     }
     
     void Rotate_Tetromino(){
@@ -287,6 +324,28 @@ public:
         return true;
     }
     
+    bool SavedTypeDoesFit(){
+        if(HasBeenPressed){
+            for(int i=0; i<SavedArraySize; i++){
+                int x=Tetromino[Saved_Type].Arr[i].x;
+                int y=Tetromino[Saved_Type].Arr[i].y;
+                
+                if((x+Pos_X)>(((((ScreenWidth()-FieldWidth)/2)-2)+Block_Size)+FieldWidth-(3*Block_Size)) or (x+Pos_X-1<(((ScreenWidth()-FieldWidth)/2)-2)) or (y+Pos_Y>((((ScreenHeight()-FieldHeight)/2)-2)+FieldHeight)-(2*Block_Size)))
+                    return false;
+                
+                for (int j=0; j<=MeshIndex;j++){
+                    MeshTetrominoArraySize = Tetromino_Mesh[MeshIndex].Arr.size();
+                    for(int k=0; k<MeshTetrominoArraySize; k++){
+                        if(y+Pos_Y+1<=Tetromino_Mesh[j].Arr[k].y+4 and y+Pos_Y+3>=Tetromino_Mesh[j].Arr[k].y){
+                            if(x+Pos_X+1<=Tetromino_Mesh[j].Arr[k].x+4 and x+Pos_X+3>=Tetromino_Mesh[j].Arr[k].x) return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
     void MoveTetromino(){
         
         if(GetKey(VK_RIGHT).bPressed and DoesPieceFit() and DoesPieceFitRight()){
@@ -361,6 +420,7 @@ public:
                         
                         Tetromino_Mesh.clear();
                         Tetromino.clear();
+                        HasBeenPressed=false;
                         cont=0;
                         Speed=60;
                         score=0;
@@ -370,8 +430,29 @@ public:
                 
             }
     
+    void SaveTetromino(){
+        if (HasBeenPressed and GetKey(VK_RSHIFT).bPressed and SavedTypeDoesFit()){
+            int n=Saved_Type;
+            Saved_Type=Tetromino_Type;
+            Tetromino_Type=n;
+        }
+        
+        if (!HasBeenPressed){
+            if(GetKey(VK_RSHIFT).bPressed){
+                
+                HasBeenPressed=true;
+                
+                Saved_Type=Tetromino_Type;
+                Tetromino_Type=Next_Type;
+                Next_Type=rand()%7;
+            }
+        }
+        
+    }
+    
     void DrawTetromino(){
         if(cont<1){
+            
             Tetromino.push_back({});
             Tetromino.push_back({});
             Tetromino.push_back({});
@@ -400,10 +481,10 @@ public:
             Tetromino[2].Arr.push_back({0,4});
             Tetromino[2].Colour=FG_DARK_BLUE;
             
-            Tetromino[3].Arr.push_back({4, 4});
-            Tetromino[3].Arr.push_back({8, 4});
-            Tetromino[3].Arr.push_back({12, 4});
+            Tetromino[3].Arr.push_back({4, 0});
+            Tetromino[3].Arr.push_back({8, 0});
             Tetromino[3].Arr.push_back({12, 0});
+            Tetromino[3].Arr.push_back({12, 4});
             Tetromino[3].Colour=FG_CYAN;
             
             Tetromino[4].Arr.push_back({8, 0});
@@ -426,7 +507,6 @@ public:
             
             
             //----------------------------------
-            
             
             Next_Tetromino.push_back({});
             Next_Tetromino.push_back({});
@@ -456,10 +536,10 @@ public:
             Next_Tetromino[2].Arr.push_back({0,4});
             Next_Tetromino[2].Colour=FG_DARK_BLUE;
             
-            Next_Tetromino[3].Arr.push_back({4, 4});
-            Next_Tetromino[3].Arr.push_back({8, 4});
-            Next_Tetromino[3].Arr.push_back({12, 4});
+            Next_Tetromino[3].Arr.push_back({4, 0});
+            Next_Tetromino[3].Arr.push_back({8, 0});
             Next_Tetromino[3].Arr.push_back({12, 0});
+            Next_Tetromino[3].Arr.push_back({12, 4});
             Next_Tetromino[3].Colour=FG_CYAN;
             
             Next_Tetromino[4].Arr.push_back({8, 0});
@@ -479,6 +559,12 @@ public:
             Next_Tetromino[6].Arr.push_back({8, 4});
             Next_Tetromino[6].Arr.push_back({12, 4});
             Next_Tetromino[6].Colour=FG_RED;
+            
+            OffSet_X_NextTetromino=((((ScreenWidth()-FieldWidth)/2)-2)+FieldWidth+Block_Size)+1;
+            OffSet_Y_NextTetromino=((ScreenHeight()-FieldHeight)/2)-2;
+            
+            OffSet_X_SavedTetromino=(((ScreenWidth()-FieldWidth)/2)-2)-(5*Block_Size);
+            OffSet_Y_SavedTetromino=((ScreenHeight()-FieldHeight)/2)-2;
         }
         
             for(int j=0;j<Tetromino[Tetromino_Type].Arr.size();j++){
@@ -504,8 +590,8 @@ public:
         }
         
             for(int j=0;j<Next_Tetromino[Next_Type].Arr.size();j++){
-                int x=Next_Tetromino[Next_Type].Arr[j].x+112;
-                int y=Next_Tetromino[Next_Type].Arr[j].y+30;
+                int x=Next_Tetromino[Next_Type].Arr[j].x+OffSet_X_NextTetromino;
+                int y=Next_Tetromino[Next_Type].Arr[j].y+OffSet_Y_NextTetromino;
                 int x1=x+Block_Size;
                 int y1=y+Block_Size;
                 short Colour=Next_Tetromino[Next_Type].Colour;
@@ -513,6 +599,34 @@ public:
                 Fill(x,y,x1,y1, PIXEL_SOLID, Colour);
             }
         
+        if(HasBeenPressed==true){
+            
+        for(int j=0;j<Next_Tetromino[Saved_Type].Arr.size();j++){
+            int x=Next_Tetromino[Saved_Type].Arr[j].x+OffSet_X_SavedTetromino;
+            int y=Next_Tetromino[Saved_Type].Arr[j].y+OffSet_Y_SavedTetromino;
+            int x1=x+Block_Size;
+            int y1=y+Block_Size;
+            short Colour=Next_Tetromino[Saved_Type].Colour;
+            
+            Fill(x,y,x1,y1, PIXEL_SOLID, Colour);
+        }
+            
+            for (int i=OffSet_Y_SavedTetromino; i<OffSet_Y_SavedTetromino+(4*Block_Size); i=i+4){
+                DrawLine(OffSet_X_SavedTetromino, i, OffSet_X_SavedTetromino+(4*Block_Size), i, FG_BLACK);
+            }
+            for (int i=OffSet_X_SavedTetromino; i<OffSet_X_SavedTetromino+(4*Block_Size); i=i+4){
+                DrawLine(i, OffSet_Y_SavedTetromino, i, OffSet_Y_SavedTetromino+(4*Block_Size), FG_BLACK);
+            }
+            
+        }
+        
+        
+        for (int i=OffSet_Y_NextTetromino; i<OffSet_Y_NextTetromino+(4*Block_Size); i=i+4){
+            DrawLine(OffSet_X_NextTetromino, i, OffSet_X_NextTetromino+(4*Block_Size), i, FG_BLACK);
+        }
+        for (int i=OffSet_X_NextTetromino; i<OffSet_X_NextTetromino+(4*Block_Size); i=i+4){
+            DrawLine(i, OffSet_Y_NextTetromino, i, OffSet_Y_NextTetromino+(4*Block_Size), FG_BLACK);
+        }
     }
     
     bool CheackToEraseCompleteLine(){
@@ -589,13 +703,16 @@ public:
     
     
     virtual bool OnUserCreate(){
-        
         return true;
     }
     
     virtual bool OnUserUpdate(float fElapsedTime){
+        PlaySample(nSample);
+        
         
         Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+        
+        SaveTetromino();
         
         if(cont_4>Speed and DoesPieceFitDown() and DoesPieceFit3()){
             Pos_Y=Pos_Y+4;
@@ -610,8 +727,6 @@ public:
         
         MeshTetrominos();
         
-        NextRotationDoesFit();
-        
         DrawTetromino();
         
         DrawField();
@@ -621,6 +736,8 @@ public:
     
         UserTetrominoArraySize = Tetromino[Tetromino_Type].Arr.size();
         MeshIndex=Tetromino_Mesh.size()-1;
+        
+        if(HasBeenPressed) SavedArraySize=Next_Tetromino[Saved_Type].Arr.size();
         
         score =Tetromino_Mesh.size();
         
